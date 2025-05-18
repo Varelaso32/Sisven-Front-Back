7afbe21ebaaf7e72e716e5047605ba40ece66b11
Varelaso32 <samyst2013@hotmail.com>
2025-05-18 14:34:52 -0500

    ✨ feat: IndexCtrller Categ
    Se agrega el index controller de categories

--- sisven/app/Http/Controllers/api/CategoriesController.php
+++ sisven/app/Http/Controllers/api/CategoriesController.php
@@ -4,44 +4,36 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Categories;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $categories = DB::table('categories')
+            ->select('categories.*')
+            ->get();
+
+        return json_encode(['categories' => $categories]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
